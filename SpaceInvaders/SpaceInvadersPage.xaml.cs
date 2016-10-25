68a21536a4d8a1b7b507148217bac953e7a61b26
sfardoux <sfardoux@maxxing.com>
2016-10-25 18:20:24 +0200

    tuning IPS resize display
    Conflicts:
    	SpaceInvaders/SpaceInvadersPage.xaml.cs

--- SpaceInvaders/SpaceInvadersPage.xaml.cs
+++ SpaceInvaders/SpaceInvadersPage.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -13,19 +13,21 @@ namespace SpaceInvaders
 		Task task;
 		Emulator emu;
 		//Emu8080 emu;
-		private const int CYCLES_PER_LOOP = 16666; // 10000000;
+		private const int CYCLES_PER_LOOP = 290000; // 16666; // 10000000;
 		private const int WIDTH = 224;
 		private const int HEIGHT = 256;
 
 		private BmpMaker bmp;
 		private ImageSource imageSource;
 		DateTime lastCycle = DateTime.Now;
+		DateTime timerFps = DateTime.Now;
 
 
 		public SpaceInvadersPage ()
 		{
 			InitializeComponent ();
 			btLaunch.Clicked += BtLaunch_Clicked;
+			btStop.Clicked += BtStop_Clicked;
 			bmp = new BmpMaker (WIDTH, HEIGHT);
 
 			for (int row = 0; row < HEIGHT; row++)
@@ -35,6 +37,32 @@ namespace SpaceInvaders
 			bmp.SetPixel (100, 100, 255, 0, 0);
 			imageSource = bmp.Generate ();
 			theImage.Source = imageSource;
+			btStop.IsEnabled = false;
+		}
+
+
+		void BtLaunch_Clicked (object sender, EventArgs e)
+		{
+			btLaunch.IsEnabled = false;
+			if (task != null) {
+				if (!task.IsCompleted) {
+					cts.Cancel ();
+					return;
+				}
+				cts = null;
+			}
+			cts = new CancellationTokenSource ();
+			action = new Action (DoRun);
+			task = new Task (action, cts.Token);
+			task.Start ();
+			btStop.IsEnabled = true;
+		}
+
+		void BtStop_Clicked (object sender, EventArgs e)
+		{
+			btStop.IsEnabled = false;
+			cts.Cancel ();
+			btLaunch.IsEnabled = true;
 		}
 
 		void Emu_OneScreen ()
@@ -43,7 +71,7 @@ namespace SpaceInvaders
 			imageSource = emu.bmp.Generate ();
 			Device.BeginInvokeOnMainThread (() => {
 				theImage.Source = imageSource;
-				lastCycle = DateTime.Now;
+				//lastCycle = DateTime.Now;
 			});
 
 		}
@@ -55,50 +83,59 @@ namespace SpaceInvaders
 
 			DateTime thisCycle;
 			TimeSpan deltaTime = new TimeSpan ();
-			int count = 0;
+			int count = 100;
 			DateTime timeInterrupt = DateTime.Now;
 
 			int toWait = 0;
 			double mhz = 2;
+<<<<<<< HEAD
 			const double MHZ = 3.5;
+=======
+			const double MHZ = 2.0;
+			const int millisSec = 50;
+			const int second = 1000;
+			const double frequency = second / millisSec;
+			const int instructionsPerFrequency = (int)(CYCLES_PER_LOOP / frequency); // * millisSec / second;
+>>>>>>> 3fde4ec... tuning IPS resize display
+
+			System.Diagnostics.Debug.WriteLine ($"Frequency: {millisSec} ms");
+			System.Diagnostics.Debug.WriteLine ($"IPF: {instructionsPerFrequency}");
+
+			lastCycle = DateTime.Now;
 
 			while (true) {
-				//emu.Execute (17000);
-				//emu.Execute (16333);
-				emu.Execute (17066);
-				//emu.Execute (17066);
-				//thisCycle = DateTime.Now;
-
-				//deltaTime = thisCycle - timeInterrupt;
-				//if (deltaTime.TotalMilliseconds > 8) {
-				//timeInterrupt = thisCycle;
-				/*
-				if (!emu.NonMaskableInterruptPending && !emu.Interrupt) {
-					interruptType = 3 - interruptType;
-					emu.InterruptMode = interruptType;
-					emu.Interrupt = true;
-				}
-				*/
-				//}
 				//emu.FetchExecute (CYCLES_PER_LOOP);
+				emu.Execute (instructionsPerFrequency);
 
-				thisCycle = DateTime.Now;
-				deltaTime = thisCycle - lastCycle;
-				count++;
-
-				imageSource = emu.bmp.Generate ();
-				Device.BeginInvokeOnMainThread (() => {
-					theImage.Source = imageSource;
-				});
-
+				// do wa have to stop?
 				if (cts != null) {
 					if (cts.IsCancellationRequested) {
 						emu = null;
+						cts = null;
 						System.Diagnostics.Debug.WriteLine ("Exit");
 						return;
 					}
 				}
-				if (deltaTime.TotalMilliseconds > 1000) {
+
+				// refresh display
+				imageSource = emu.bmp.Generate ();
+				Device.BeginInvokeOnMainThread (() => {
+					theImage.Source = imageSource;
+					count++;
+				});
+
+				// calculate elaps time
+				thisCycle = DateTime.Now;
+				deltaTime = thisCycle - lastCycle;
+
+				// calculate the slowdown
+				if (deltaTime.TotalMilliseconds < millisSec) {
+					toWait = (millisSec - (int)deltaTime.TotalMilliseconds) / 10;
+				} else toWait = 0;
+
+				// display FPS
+				if ((thisCycle - timerFps).TotalMilliseconds > 1000) {
+					/*
 					mhz = count * 33.333 / (deltaTime).TotalMilliseconds;
 					if (mhz > MHZ) {
 						if (mhz - MHZ > 1000)
@@ -112,33 +149,24 @@ namespace SpaceInvaders
 					}
 					if (mhz < MHZ)
 						toWait--;
-					System.Diagnostics.Debug.WriteLine (string.Format ("Running at ~{0:N2} MHz ({1} fps - wait {2})", count * 33.333 / (deltaTime).TotalMilliseconds, count, toWait));
-					lastCycle = thisCycle;
+						*/
+					//System.Diagnostics.Debug.WriteLine (string.Format ("Running at ~{0:N2} MHz ({1} fps - wait {2})", count * 33.333 / (deltaTime).TotalMilliseconds, count, toWait));
+					//System.Diagnostics.Debug.WriteLine (string.Format ("Running at ~{0:N2} MHz ({1} fps - wait {2})", count * 33.333 / (deltaTime).TotalMilliseconds, count, toWait));
+					System.Diagnostics.Debug.WriteLine (string.Format ("{0} fps - wait {1}", count, toWait));
+					timerFps = thisCycle;
 					count = 0;
-
 				}
 
+				// do we have to slowdown?
 				if (toWait > 0)
 					await Task.Delay (toWait);
+
+				lastCycle = DateTime.Now;
 
 			} // while
 		}
 
 
-		void BtLaunch_Clicked (object sender, EventArgs e)
-		{
-			if (task != null) {
-				if (!task.IsCompleted) {
-					cts.Cancel ();
-					return;
-				}
-				cts = null;
-			}
-			cts = new CancellationTokenSource ();
-			action = new Action (DoRun);
-			task = new Task (action, cts.Token);
-			task.Start ();
-		}
 
 	}
 }
